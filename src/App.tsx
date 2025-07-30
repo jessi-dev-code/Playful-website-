ca7564b6b74866067127d02c5236b923fd944526
jaspreet <jaspreet.bhagat@ensuesoft.com>
2025-07-30 14:26:39 +0530

    added files to git

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Header from "./Components/Header";
+import Newsletters from "./Components/Newsletters";
+function App() {
+  return (
+    <>
+    <Newsletters />
+    <Header />
+    </>
+  );
+}
+
+export default App;
